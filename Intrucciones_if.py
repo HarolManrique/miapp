b3a1b9a3d489144bfaa77b5badc27841a40bb5cc
Harol Manrique <hhmanrique95@gmail.com>
2023-08-31 17:27:27 -0500

    instruccion if

--- Intrucciones_if.py
+++ Intrucciones_if.py
@@ -1,4 +1,4 @@
-var_digitada = input("Digite su edad:  ")
+"""var_digitada = input("Digite su edad:  ")
 var_edad = int(var_digitada)
 if(not(var_digitada =="")):
     if(var_edad < 0):
@@ -13,7 +13,7 @@ else:
 if(var_edad < 0 or var_edad == 0):
         print("edad incorrecta, no puede ser negativa")
 elif(var_edad > 0 and var_edad < 18):
-    print("es niño o joven no mayor de edad")
+    print("es niño o joven no mayor de edad")"""
 
 
 variable_digitada = input("Digite su edad:")
